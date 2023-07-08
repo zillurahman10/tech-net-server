3797407f85006bc1c715dd33eb3beb62eab216aa
zillurahman10 <zillur.hero40@gmail.com>
2023-07-08 20:35:48 +0600

    initialize done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.euxm4cs.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://zillurhero40:NxS9eM4D3EpHRuuu@cluster0.uc15omt.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
